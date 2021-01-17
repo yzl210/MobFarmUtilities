6b9c367b963a12fcea6e752c658ced70e7bea324
yzl210 <2846840578@qq.com>
2021-01-17 12:32:04 +0800

    Initial commit

---
+++ src/main/java/cn/leomc/mobfarmutilities/MobFarmUtilities.java
@@ -0,0 +1,31 @@
+package cn.leomc.mobfarmutilities;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(MobFarmUtilities.MODID)
+public class MobFarmUtilities {
+
+    public static final String MODID = "mobfarmutilities";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public MobFarmUtilities() {
+    }
+
+}
